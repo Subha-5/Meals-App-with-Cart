019b6208d3f4486acced6b89a98918025c0289d4
Subha Sadhu <subhasadhu5@gmail.com>
2023-04-29 00:45:51 +0530

    completing basic layout

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -6,7 +6,7 @@ const HeaderCartButton = () => {
     <button className={classes.button}>
         <span className={classes.icon}><CartIcon /></span>
         <span>Your Cart</span>
-        <span className={classes.badge}>1</span>
+        <span className={classes.badge}>0</span>
     </button>
   )
 }
---
+++ src/components/Meals/AvailableMeals.jsx
@@ -0,0 +1,25 @@
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import DUMMY_MEALS from "../data/dummy-meals.js";
+import MealItem from "./MealItem/MealItem";
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal, key) => (
+    <MealItem
+      key={meal.id}
+      id = {meal.id}
+      name={meal.name}
+      price={meal.price}
+      description={meal.description}
+    />
+  ));
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
