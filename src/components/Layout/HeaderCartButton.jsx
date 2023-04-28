d2742e24824068913b75c7c5f0dc11bb98772065
Subha Sadhu <subhasadhu5@gmail.com>
2023-04-29 03:36:11 +0530

    Adding and using Cart Context

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,14 +1,23 @@
-import classes from './HeaderCartButton.module.css'
-import CartIcon from "../Cart/CartIcon"
+import { useContext } from "react";
 
-const HeaderCartButton = ({onClick}) => {
+import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
+
+const HeaderCartButton = ({ onClick }) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => {
+    currentNum + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={onClick}>
-        <span className={classes.icon}><CartIcon /></span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>0</span>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
-  )
-}
+  );
+};
 
-export default HeaderCartButton
+export default HeaderCartButton;
